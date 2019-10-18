cc0042108936aa2ca5910581dbf451e2982a70e7
Circuandante <circuandante@gmail.com>
2019-10-18 14:55:16 -0500

    crear views y routs

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,28 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.use(VueRouter)
+import whoAMi from '../views/aboutMe.vue';
+import contact from '../views/contact.vue';
+
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
   {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  { path: '/', name: 'aboutMe', component: whoAMi },
+  { path: '/contact', name: 'Contact', component: contact },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/aboutMe.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <figure class="me" id="me"><img src="" alt="" class="meImg"></figure>
+        <ul id="Menu" class="menu">
+            <li v-show="!menu">Inicio</li>
+            <li v-show="menu">Portafolio</li>
+            <li>Contacto</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+    export default {
+        computed: {
+            ...mapState(['menu'])
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
